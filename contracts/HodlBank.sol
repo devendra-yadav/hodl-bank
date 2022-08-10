6e8b32daee2224b99299e45be9a971fe9eafb877
Devendra Yadav <Devendra_Yadav@epam.com>
2022-08-10 14:01:52 +0800

    simplied logic

--- contracts/HodlBank.sol
+++ contracts/HodlBank.sol
@@ -2,61 +2,63 @@
 
 pragma solidity ^0.8.9;
 
+/**
+ * @title This contract is a HODL bank where users can HODL thier tokens and cant withdraw before the HODL time is over
+ * @author Devendra Yadav(devendra.yadav@gmail.com)
+ * 
+ */
 contract HodlBank {
 
     address payable public owner;
     address payable public contractAddress;
 
-    //Contains userAddress mapping to array of tokenContractAddress HODLed
-    private mapping(address=>address[]) userHoldTokenMap;
-
-    private struct HodlData{
-        uint startTime,
-        uint amount,
-        uint endTime
+    
+    struct HodlData{
+        address tokenContractAddress;
+        uint startTime;
+        uint amount;
+        uint endTime;
     }
 
-    //Mapping of user address =>  Array of mapping of token contract address/HODL data struct
-    private mapping (address => mapping(address=> HodlData[])) userHodlTokenDetails;
-
+    //Contains userAddress mapping to array of HodlData[]
+    mapping(address=>HodlData[]) public userHoldTokenMap;
 
     event HodlInitiated(address indexed requester, uint startDateTime, uint endDateTime, address tokenContractAddress, uint amount);
 
     event Withdraw(address indexed requester, uint timestamp, string tokenName, uint amount);
 
+    event HoldRequestFailed(address indexed requester, uint startDateTime, uint endDateTime, address tokenContractAddress, uint amount);
+
     constructor() {
-        owner=msg.sender;
+        owner=payable(msg.sender);
         contractAddress=payable(address(this));
     }
 
-    function hodlRequest(address requester, address tokenAddress, uint amount, uint hodlEndTime) public payable returns (boolean result) {
-        
-        //Get all Hodled tokens of the requester
-        address[] holdTokens = userHoldTokenMap.get(requester);
+    /**
+     * @dev : This function is to initial the hodl request by a user for a specific token for a given amount.
+     *        it also emit the corresponding event for logging purpose.
+     * @param requester  Requester address who want to HODL
+     * @param tokenAddress  contract address of the token that the user want to hODL
+     * @param amount  amount of tokens to HODL
+     * @param hodlEndTime  end time of the HODL. Amount can be withdrawn on after this time.
+     * @return result  it returns if the HODL request was successful or it failed.
+     */
+    function hodlRequest(address requester, address tokenAddress, uint amount, uint hodlEndTime) public payable returns (bool result) {
 
-        //Checking if requester is already HOLDing this token. if no then add it to the user=>token[] map (userHoldTokenMap)
-        boolean tokenAlreadyHodled=false;
-        for(int i=0;i<holdTokens.length;i++){
-            address hodledTokenAddress=holdTokens.get(i);
-            if(hodledTokenAddress == tokenAddress){
-                tokenAlreadyHodled=true;
-                userHoldTokenMap[requester].push(tokenAddress);
-                break;
-            }
-        }
+      
+        if(userHoldTokenMap[requester].length == 0){
+            
+            userHoldTokenMap[requester]=[HodlData(tokenAddress, block.timestamp, amount, hodlEndTime)];
+        }else{
+            //push HodlData to the existing array for the requester
+            userHoldTokenMap[requester].push(HodlData(tokenAddress, block.timestamp, amount, hodlEndTime));
 
-        if(!tokenAlreadyHodled){
-            address[] tokenAddresses;
-            hodlDtokenAddressesata.push(tokenAddress);
-            userHoldTokenMap[requester]=tokenAddresses;
-        }
-
-        //Create a HodlData struct
-        HodlData hodlData=HodlData(block.timestamp, amount, hodlEndTime);
-        mapping(address=>HodlData[]) hodlDataMap=userHodlTokenDetails.get(requester);
-
-        
-
+        }     
+    
         emit HodlInitiated(requester, block.timestamp, hodlEndTime, tokenAddress, amount );
+
+       
+        
+        return true;
     }
 }
